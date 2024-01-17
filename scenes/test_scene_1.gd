066f43c9e389e845875461f71641307b199e64b7
brycekim2312 <156628538+brycekim2312@users.noreply.github.com>
2024-01-17 04:56:46 -0600

    coal created

--- scenes/test_scene_1.gd
+++ scenes/test_scene_1.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+var throw_coal_scene: PackedScene = preload("res://scenes/projectiles/parabola_coal.tscn")
+
+func _on_santa_throw(pos):
+	var pcoal = throw_coal_scene.instantiate()
+	pcoal.position = pos
+	add_child(pcoal)
+	
 func _ready():
 	pass
 	
 func _process(delta):
 	pass
+
+
+func _on_santa_missile():
+	pass # Replace with function body.
