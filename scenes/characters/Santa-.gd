96cfeef99e579dfaa949206914ca17b7eae01a20
brycekim2312 <156628538+brycekim2312@users.noreply.github.com>
2024-01-17 03:02:51 -0600

    jump

---
+++ scenes/characters/Santa-.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+#var velocity = Vector2.ZERO
+
+const Gravity = 20
+const jumpspeed = 700
+const floor = Vector2(0,-1)
+const horizspeed = 300
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#pos = Vector2(38,596)
+	position = Vector2(38,596)
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	if Input.is_action_pressed("left"):
+		velocity.x -= horizspeed*delta
+	elif Input.is_action_pressed("right"):
+		velocity.x += horizspeed*delta
+	else:
+		velocity.x = 0
+	
+	
+	if Input.is_action_just_pressed("up"):
+		if is_on_floor():
+			velocity.y = -jumpspeed
+	velocity.y+=Gravity
+	
+	move_and_slide()
+	
+	
+	#position += velocity*delta
+	
+		
+	pass
