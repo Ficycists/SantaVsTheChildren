e241429ec32c2fe33ed0b7a7db2af683d7e1a53a
Viola Case <ajr.acnt@gmail.com>
2024-01-21 00:20:59 -0600

    .

---
+++ scenes/controllers/testscene_control.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
+
+# Assuming you are in a script attached to a node in the scene
+
+# Change to a new scene by providing the path to the scene
+func change_scene() -> void:
+	# Get the current SceneTree
+	var tree = get_tree()
+	
+	# Load the new scene
+	var new_scene = preload("res://scenes/test_scene_2.tscn")
+	
+	# Change the scene
+	tree.change_scene(new_scene)
