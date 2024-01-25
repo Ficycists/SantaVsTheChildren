c2b4cf575aed54fa362d26ca3dad009748d5dcd6
Nacho1708 <iaq1@rice.edu>
2024-01-25 02:30:22 -0600

    Fixed (zombies on wall not anymore)

--- scenes/test_scene_1.gd
+++ scenes/test_scene_1.gd
@@ -135,13 +135,13 @@ func _on_zombie_timer_timeout():
 	#if did_santa_die==false:
 	var right_zombie = right_side_zombies.instantiate() as CharacterBody2D
 	var left_zombie = left_side_zombies.instantiate() as CharacterBody2D
+	left_zombie.position.x = $Santa.position.x - 500 + randi_range(-10,10)
 	right_zombie.position.x = $Santa.position.x + 500 + randi_range(-10,10)
 	right_zombie.position.y = $Santa.position.y-200
-	left_zombie.position.x = $Santa.position.x - 500 + randi_range(-10,10)
-	right_zombie.position.y = $Santa.position.y-200
-	$ZOMBIES/new_zombies.add_child(right_zombie)
 	$ZOMBIES/new_zombies.add_child(left_zombie)
 	left_zombie.add_to_group("Left Zombies")
+	if !right_zombie.is_on_wall():	
+		$ZOMBIES/new_zombies.add_child(right_zombie)
 	#print('new-zombie')
 	$ZOMBIES/Zombie_timer.wait_time=zombie_rate
 	$ZOMBIES/Zombie_timer.start()
